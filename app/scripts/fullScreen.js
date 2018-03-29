284f2d396fd38c005fe4fef539219570d3ab31ad
vtimsit <victor.timsit@hetic.net>
2018-03-29 15:19:28 +0200

    :sparkles: Project full screen

--- app/scripts/fullScreen.js
+++ app/scripts/fullScreen.js
@@ -1,19 +1,25 @@
 class FullScreen {
-    constructor(fullScreenIconClass, boxClass) {
-        this.fullScreenIcons = document.querySelectorAll(fullScreenIconClass)
-        this.box = document.querySelector(boxClass)
+    constructor(fullScreenIconClass, fullScreenExitIconClass, projectBoxClass, contentBoxClass) {
+        this.fullScreen = document.querySelectorAll(fullScreenIconClass)
+        this.fullScreenExit = document.querySelectorAll(fullScreenExitIconClass)
+        this.projectBox = document.querySelectorAll(projectBoxClass)
+        this.contentBox = document.querySelectorAll(contentBoxClass)
 
-        for(const fullScreenIcon of this.fullScreenIcons) {
-            fullScreenIcon.addEventListener('click', () => {
-                this.fullScreen()
+        for(let i = 0; i < this.fullScreen.length; i++) {
+            this.fullScreen[i].addEventListener('click', () => {
+                this.fullScreenToggle(this.fullScreen, this.fullScreenExit, this.projectBox, this.contentBox, i)
+            })
+            this.fullScreenExit[i].addEventListener('click', () => {
+                this.fullScreenToggle(this.fullScreen, this.fullScreenExit, this.projectBox, this.contentBox, i)
             })
         }
     }
 
-    fullScreen() {
-        this.box.classList.toggle('project__content--fullScreen')
-        for(const fullScreenIcon of this.fullScreenIcons) {
-            fullScreenIcon.classList.toggle('project__fullScreen--active')
-        }
+    fullScreenToggle(fullScreen, fullScreenExit, projectBox, contentBox, i) {
+        console.log(contentBox)
+        fullScreen[i].classList.toggle('project__fullScreen--active')
+        fullScreenExit[i].classList.toggle('project__fullScreen--active')
+        projectBox[i].classList.toggle('project__content--fullScreen')
+        contentBox[i].classList.toggle('project__content--fullScreen')       
     }
 }
